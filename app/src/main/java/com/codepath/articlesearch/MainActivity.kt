1869aa16a3e6ba2ace59191a0753674318a02f25
Bach Nguyen-Ngo <bachnguyenngo2000@outlook.com>
2024-04-03 11:48:52 -0500

    finished lab 6

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -3,12 +3,15 @@ package com.codepath.articlesearch
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.articlesearch.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
@@ -32,15 +35,34 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        // Call helper method to swap the FrameLayout with the fragment
-        replaceFragment(ArticleListFragment())
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        // define your fragments here
+        val bestSellerBooksFragment: Fragment = BestSellerBooksFragment()
+        val articleListFragment: Fragment = ArticleListFragment()
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        // handle navigation selection
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            lateinit var fragment: Fragment
+            when (item.itemId) {
+                R.id.nav_books -> fragment = bestSellerBooksFragment
+                R.id.nav_articles -> fragment = articleListFragment
+            }
+            // Call helper method to swap the FrameLayout with the fragment
+            replaceFragment(fragment)
+            true
+        }
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.nav_books
     }
 
-    //  a helper method within MainActivity to be able to add swap the fragment
-    private fun replaceFragment(articleListFragment: ArticleListFragment) {
+    //  a helper method within MainActivity to be able to swap the fragment
+    private fun replaceFragment(fragment: Fragment) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.article_frame_layout, articleListFragment)
+        fragmentTransaction.replace(R.id.article_frame_layout, fragment)
         fragmentTransaction.commit()
     }
 }
